90b468da9da494dc96683c517945bd2224dae386
jacobcobler <jxcobler@gmail.com>
2026-02-05 23:40:52 -0500

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,20 +1,34 @@
 import cv2
+import time
 
-classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+classifier = cv2.CascadeClassifier("haarcascade_fullbody.xml")
+if (classifier.empty):
+        raise IOError("Error loading haarcascade_fullbody.xml")
+
 captureDevice = cv2.VideoCapture(0)
 
+t1 = time.time()
 while True:
     rec, image = captureDevice.read()
+    if not rec:
+         break
+    
+    #Resize image
+    image = cv2.resize(image, (640, 480))
+    #Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     seq = classifier.detectMultiScale(gray, 1.05,6)
 
     for(x,y,w,h) in seq:
         cv2.rectangle(image,(x,y),(x+w, y+h), (255,0,0), 5)
-        print("found")
+        # GPIO.output(18,GPIO.HIGH)
+        t2 = time.time()
+        if (t2 - t1) > 1:
+            print("found")
+            t1 = time.time()
 
     cv2.imshow('img', image)
-    delay = cv2.waitKey(10) & 0xff
-    if delay == 10:
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
 captureDevice.release()
